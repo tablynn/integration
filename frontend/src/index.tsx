9ab7fc845aab0f518e8a6e025ae0445e1437d271
Emily Perelman <emilyperelman@emilys-mbp-88.devices.brown.edu>
2022-11-21 22:03:42 -0500

    trying to react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,19 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/foo">Foo</Link>
+          <Link to="/bar">Bar</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/foo" component={Foo} />
+          <Route exact path="/bar" component={Bar} />
+        </Switch>
+      </div>
+    </Router>
 );
