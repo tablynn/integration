5f6f87a3bb9574e883cbf14c3af776576dfa51bc
Emily Perelman <emilyperelman@emilys-mbp-88.devices.brown.edu>
2022-11-21 22:04:07 -0500

    trying to do react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,20 +7,20 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  return (
-    <Router>
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/foo">Foo</Link>
-          <Link to="/bar">Bar</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/foo" component={Foo} />
-          <Route exact path="/bar" component={Bar} />
-        </Switch>
-      </div>
-    </Router>
-);
+// root.render(
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <Link to="/">Home</Link>
+//           <Link to="/foo">Foo</Link>
+//           <Link to="/bar">Bar</Link>
+//         </nav>
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/foo" component={Foo} />
+//           <Route exact path="/bar" component={Bar} />
+//         </Switch>
+//       </div>
+//     </Router>
+// );
